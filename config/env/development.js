6dc3efe0ee8f3405b5f7ff72bc169fe3db3ee936
EZKOW <eloik.rousseau@gmail.com>
2022-08-30 18:24:25 -0400

    Updated mongodb link in development.js

--- config/env/development.js
+++ config/env/development.js
@@ -6,6 +6,8 @@ module.exports = {
     port: process.env.PORT || 3000,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb://localhost/pisignage-dev'
+            'mongodb+srv://ezkow:4590@pisignage.gni79rg.mongodb.net/?retryWrites=true&w=majority'
+
+'
     }
 };
