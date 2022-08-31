5f26d71f782603c022843f25c79cb4147ee4aebe
EZKOW <eloik.rousseau@gmail.com>
2022-08-30 20:45:58 -0400

    Updated url

--- config/env/development.js
+++ config/env/development.js
@@ -6,8 +6,6 @@ module.exports = {
     port: process.env.PORT || 3000,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb+srv://ezkow:4590@pisignage.gni79rg.mongodb.net/?retryWrites=true&w=majority'
-
-
+            'mongodb://ezkow:4590@ac-ev8m1nw-shard-00-00.gni79rg.mongodb.net:27017,ac-ev8m1nw-shard-00-01.gni79rg.mongodb.net:27017,ac-ev8m1nw-shard-00-02.gni79rg.mongodb.net:27017/?ssl=true&replicaSet=atlas-ls58ts-shard-0&authSource=admin&retryWrites=true&w=majority'
     }
 };
