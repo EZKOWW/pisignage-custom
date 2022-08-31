9a53eb102f8e08c515339603ad6b8e41ef669fbf
EZKOW <eloik.rousseau@gmail.com>
2022-08-30 21:03:29 -0400

    CHanged whole url

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ var path = require('path'),
 
 // Application Config
 var config = require(path.join(__dirname,'/config/config'));
+var atlasURL = 'mongodb://ezkow:4590@ac-ev8m1nw-shard-00-00.gni79rg.mongodb.net:27017,ac-ev8m1nw-shard-00-01.gni79rg.mongodb.net:27017,ac-ev8m1nw-shard-00-02.gni79rg.mongodb.net:27017/?ssl=true&replicaSet=atlas-ls58ts-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 // Connect to database
 mongoose.Promise = global.Promise;
-mongoose.connect(config.mongo.uri,function(error){
+mongoose.connect(atlasURL, function(error){
     if (error) {
 	console.log(error)
         console.log('********************************************');
